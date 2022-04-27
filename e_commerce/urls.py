d856dca276d477aedf4571cd212febd01ac230f1
armarcm <armarcm@gmail.com>
2022-04-27 19:53:00 -0300

    inclusao de arquivos estaticos

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_page, about, contact, login_page, register_page
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home_page),
@@ -26,3 +28,8 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + \
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + \
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
